04512ed8fe1b38ed92b5eaa2c3b5e8adc73c3463
Sharick Huasco <sharick.huasco.pure@vallegrande.edu.pe>
2024-10-08 21:24:16 -0500

    prototipo

--- scrip.js
+++ scrip.js
@@ -1,8 +1,7 @@
-// Seleccionamos los elementos del menú burger
+
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
 
-// Agregamos el evento al hacer clic en el menú
 burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
 });
